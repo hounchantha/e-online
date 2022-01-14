58f3e6ffd7c5b1f7c1a9a6edda351ec02559d94a
Chantha <hounchantha@gmail.com>
2022-01-14 12:41:00 +0700

    pust final project to github

---
+++ index.php
@@ -0,0 +1,423 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <title>Chetra  &amp; Muytoung - eWedding Invitation</title>
+  <link rel="icon" type="image/png"  href="img/favicon.png">
+  <meta name="description" content="Invitation">
+  <meta name="robots" content="nofollow, noindex, noarchive, noimageindex">
+  <meta property="og:locale" content="en_US">
+  <meta property="og:type" content="article">
+  <meta property="og:title" content="Sambot.Online">
+  <meta property="og:description" content="Invitation">
+  <meta property="og:url" content="http://sambot.online/virya_meardey/">
+  <meta property="og:site_name" content="e-invitation">
+  <meta property="article:tag" content="protokol indonesia">
+  <meta property="article:section" content="Desain 9 - Demo">
+  <meta property="og:updated_time" content="2021-03-11T18:34:05+07:00">
+  <meta property="og:image" content="/tomas/3.jpg">
+  <meta property="og:image:secure_url" content="/tomas/3.jpg">
+  <meta property="og:image:width" content="1067">
+  <meta property="og:image:height" content="1600">
+  <meta property="og:image:alt" content="Sambot.Onlin">
+  <meta property="og:image:type" content="image/jpeg">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:title" content="Sambot.Onlin">
+  <meta name="twitter:description" content="Invitation">
+  <meta name="twitter:image" content="/tomas/3.jpg">
+  <!-- /Rank Math WordPress SEO plugin -->
+
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/flexslider/2.5.0/flexslider.min.css" rel="stylesheet"> -->
+
+  <!-- AddEvent theme css -->
+  <!-- <link rel="stylesheet" href="css/theme4.css" type="text/css" media="screen" /> -->
+  <!-- AddEvent -->
+  <!-- <script type="text/javascript" src="https://addevent.com/libs/atc/1.6.1/atc.min.js"></script> -->
+
+  <link href="css/popup-slider.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <link href="venobox/venobox.min.css" rel="stylesheet">
+</head>
+<body>
+  <div class="black"></div>
+  <div class="backed"></div>
+  <article class="wrapper"></article>
+
+  <section class="culture-section">
+    <div class="container">
+        <p style="padding:0; margin:10px auto;"><img style="width:240px;" src="tomas/txtname.png"></p>
+        <span class="btn btn-slider">បើកសំបុត្រអញ្ជើញ</span>
+    </div>
+  </section>
+
+  <article class="display"> 
+    <section id="home" class="home quote ct-sliderPop-close" style="margin-top: 0px;">
+      <div>
+          <div class="audio">
+            <span id="stop" class="btn stop_mp3"><img src="img/stop.png" /></span>
+            <span id="start" class="btn start_mp3"><img src="img/play.png"></span>
+          </div>
+          <div class="sliderPop"> 
+            <img src="img/popup.png" />
+            <!-- <i class="fa fa-code" aria-hidden="true"></i> -->
+            <p style="padding: 30px; color: #d6c28c; cursor: pointer;">ចុចទីនេះដើម្បីមើលពត៌មានលំអិត</p>
+            <a class="ct-newsletter-close ct-sliderPop-close" href="#">
+              <i class="fa fa-window-close"></i>
+            </a>
+          </div>
+          
+          <div class="pic">
+            <!-- <img with="100%" src="img/F1.png" /> -->
+            <img with="100%" src="tomas/4.jpg" />
+          </div>
+      </div>
+    </section>
+    <section id="event" class="home quote1">
+      <div class="container">
+          <div class="pic js-scroll fade-in-bottom">
+            <img with="100%" src="tomas/txt1.png" />
+          </div>
+      </div>
+    </section>
+    <section class="home quote1">
+      <div class="container">
+          <div class="pic js-scroll fade-in-bottom">
+            <img with="100%" src="tomas/txt2.png" />
+          </div>
+      </div>
+    </section>
+    <section  class="home quote">
+      <div class="container">      
+          <div class="pic js-scroll fade-in-bottom">
+            <img with="100%" src="tomas/txt3.png" />
+          </div>
+      </div>
+    </section>
+    <section  class="home quote">
+      <div class="container">      
+          <div class="pic js-scroll fade-in-bottom">
+            <img with="100%" src="tomas/txt3.1.png" />
+          </div>
+      </div>
+    </section>
+    <section  class="home quote">
+      <div class="container">      
+          <div class="pic js-scroll fade-in-bottom">
+            <img class="txt" with="100%" src="tomas/txt4.png" />
+          </div>
+      </div>
+    </section>
+    <section  class="home quote">
+      <div class="container">      
+          <div class="pic js-scroll fade-in-bottom">
+            <img class="txt" with="100%" src="tomas/txt5.png" />
+          </div>
+          <p style="padding: 30px 0; margin-top: -65px; z-index: 99999; position: relative;">
+            <a style="color: #be872b; font-weight: bold;" href="#location">ចុចទីនេះដើម្បីមើលទីតាំង</a>
+          </p>
+      </div>
+    </section>
+
+
+    <section id="couple" class="home quote">
+      <div class="container">    
+          <div class="pic js-scroll fade-in-bottom">
+            <img with="100%" src="tomas/cp1.png" />
+          </div>
+      </div>
+    </section>
+
+    <section id="location" class="home quote wish maps">
+      <div class="container js-scroll fade-in-bottom"> 
+          <div class="ending js-scroll fade-in-bottom">
+            <p><img style="width:100%;" src="tomas/qt2.png"></p>
+          </div>
+
+           <!-- <span><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3908.5756390015367!2d104.90080941480812!3d11.58224919177782!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x310951f78bcb1799%3A0xf48a30b3fde1e6d4!2sThe%20Grand%20Terrace!5e0!3m2!1sen!2sus!4v1636633752621!5m2!1sen!2sus" width="600" height="450" style="border:0; width: 90%; height: 334px; border: 4px solid #f3c072;" allowfullscreen="" loading="lazy"></iframe></span> -->
+
+           <span>
+             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3909.079274777144!2d104.93120651549695!3d11.546170947668903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3109513dc76a6be3%3A0x751bbde6c426d379!2sSofitel%20Phnom%20Penh%20Phokeethra!5e0!3m2!1sen!2skh!4v1641716231317!5m2!1sen!2skh" width="600" height="450" style="border:0; width: 90%; height: 334px; border: 4px solid #f3c072;" allowfullscreen="" loading="lazy"></iframe>
+           </span>
+
+      </div>
+    </section>
+
+    <section id="gallery"  class="home quote">
+      <div class="container">   
+          <div class="pic gallery js-scroll fade-in-bottom">
+            <a class="venobox vbox-item" data-gall="gall1" href="demopic/1.jpg">
+              <img with="100%" src="demopic/1.jpg" />
+            </a>
+          </div>
+
+          <div class="pic gallery js-scroll fade-in-bottom">
+            <a class="venobox vbox-item" data-gall="gall1" href="demopic/2.jpg">
+              <img with="100%" src="demopic/2.jpg" />
+            </a>
+          </div>
+          <div class="pic gallery js-scroll fade-in-bottom">
+            <a class="venobox vbox-item" data-gall="gall1" href="demopic/3.jpg">
+              <img with="100%" src="demopic/3.jpg" />
+            </a>
+          </div>
+          <div class="pic gallery js-scroll fade-in-bottom">
+            <a class="venobox vbox-item" data-gall="gall1" href="demopic/4.jpg">
+              <img with="100%" src="demopic/4.jpg" />
+            </a>
+          </div>
+          <div class="pic gallery js-scroll fade-in-bottom">
+            <a class="venobox vbox-item" data-gall="gall1" href="demopic/5.jpg">
+              <img with="100%" src="demopic/5.jpg" />
+            </a>
+          </div>
+          <div class="pic gallery js-scroll fade-in-bottom">
+            <a class="venobox vbox-item" data-gall="gall1" href="demopic/6.jpg">
+              <img with="100%" src="demopic/6.jpg" />
+            </a>
+          </div>
+          <div class="pic gallery js-scroll fade-in-bottom">
+            <a class="venobox vbox-item" data-gall="gall1" href="demopic/7.jpg">
+              <img with="100%" src="demopic/7.jpg" />
+            </a>
+          </div>
+          <div class="pic gallery js-scroll fade-in-bottom">
+            <a class="venobox vbox-item" data-gall="gall1" href="demopic/8.jpg">
+              <img with="100%" src="demopic/8.jpg" />
+            </a>
+          </div>
+          <div class="pic gallery js-scroll fade-in-bottom">
+            <a class="venobox vbox-item" data-gall="gall1" href="demopic/9.jpg">
+              <img with="100%" src="demopic/9.jpg" />
+            </a>
+          </div>
+          <div class="pic gallery js-scroll fade-in-bottom">
+            <a class="venobox vbox-item" data-gall="gall1" href="demopic/10.jpg">
+              <img with="100%" src="demopic/10.jpg" />
+            </a>
+          </div>
+          <div class="pic gallery js-scroll fade-in-bottom">
+            <a class="venobox vbox-item" data-gall="gall1" href="demopic/11.jpg">
+              <img with="100%" src="demopic/11.jpg" />
+            </a>
+          </div>
+      </div>
+    </section>
+
+    <section id="quote" class="home quote wish">
+      <div class=" container">    
+          <div class="ending js-scroll fade-in-bottom">
+            <!-- <p>វត្តមានរបស់ភ្ញៀវកិត្តិយសទាំងអស់<br>គឺជាកិត្តិយសដ៍ខ្ពង់ខ្ពស់របស់គ្រួសារពួកយើងទាំងពីរ</p> -->
+            <p><img style="width:90%;" src="tomas/qt.png"></p>
+            <span>Chetra  &amp; Muytoung</span>
+          </div>
+          <div class="pic js-scroll fade-in-bottom" style="z-index:99999; padding: 50px 30px 0px 30px;">
+             <a class="btn btn-danger venoboxvid vbox-item video" data-gall="gall-video" data-autoplay="true" data-vbtype="video" href="https://youtu.be/mKRQLb7rYzI">
+                <img class="youtube" src="tomas/vdo.jpg">
+              </a>
+          </div>
+          <div class="pic js-scroll fade-in-bottom" style="z-index:99999; margin-top: 20px; padding: 0px 30px 200px 30px;">
+             <a class="btn btn-danger venoboxvid vbox-item video" data-gall="gall-video" data-autoplay="true" data-vbtype="video" href="https://youtu.be/4XrKOJW_HaI">
+                <img class="youtube" src="tomas/vdo2.jpg">
+              </a>
+          </div>
+      </div>
+    </section>
+
+    <section id="wish" class="home last quote wish maps">
+      <div class="container js-scroll fade-in-bottom"> 
+          <div class="ending js-scroll fade-in-bottom">
+            <h3 style="font-weight: bold;font-size: 36px;text-transform: uppercase;color:#f3bf72;">
+              <p><img style="width:unset;" src="tomas/qt3.png"></p>
+            </h3>
+            <span>Chetra  &amp; Muytoung</span>
+          </div>
+          <iframe src="test.php" title="comment" style="border:0; width: 85%; height: 1280px; word-break: break-all;"></iframe>
+			<div class="ending js-scroll fade-in-bottom">
+	            <span>Thanks You</span>
+              <p style="color: #f3bf72;">Powered by <br />
+                <a style="color: #f3bf72;" href="http://sambot.online/" target="_blank">Sambot Online</a> & <a style="color: #f3bf72;" href="https://www.facebook.com/perfectweddinggiftt" target="_blank">Perfect Wedding</a>
+              </p>
+	          </div>
+      </div>
+    </section>
+
+    <section class="nav-footer">
+      <div class="nav">
+        <ul>
+          <li>
+            <a href="#event" class="nav-link">
+              <span><img src="img/event.png" alt="event" /></span><br>
+              <span class="icon-home">កម្មវិធី</span>
+            </a>
+          </li>
+          <li>
+            <a href="#location" class="nav-link">
+              <span><img src="img/couple.png" alt="couple" /></span><br>
+              <span class="icon-home">ទីតាំង</span>
+            </a>
+          </li>
+          <li>
+            <a href="#gallery" class="nav-link">
+              <span><img src="img/gallery.png" alt="gallery" /></span><br>
+              <span class="icon-home">រូបភាព</span>
+            </a>
+          </li>
+          <li>
+            <a href="#quote" class="nav-link">
+              <span><img src="img/quote.png" alt="quote" /></span><br>
+              <span class="icon-home">វីឌីអូ</span>
+            </a>
+          </li>
+          <li>
+            <a href="#wish" class="nav-link">
+              <span><img src="img/wish.png" alt="wish" /></span><br>
+              <span class="icon-home">ជូនពរ</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </article>
+
+  <footer>     
+
+    <audio id="song" loop="">
+      <source src="mp3/audio.mp3" type="audio/mp3">
+    </audio>  
+
+      <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha384-vk5WoKIaW/vJyUAd9n/wmopsmNhiy+L2Z+SBxGYnUkunIxVxAv/UtMOhba/xskxh" crossorigin="anonymous"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/flexslider/2.5.0/jquery.flexslider-min.js"></script>
+      <script src="js/popup-slider.js"></script>
+      <script src="venobox/venobox.min.js"></script>
+      <script>
+          $('.start_mp3').hide();
+          jQuery('.btn-slider').on('click',function(){
+            document.getElementById("song").play();
+          });
+          jQuery('.stop_mp3').on('click',function(){
+            $('.start_mp3').show();
+            $('.stop_mp3').hide();
+            document.getElementById("song").pause();
+          });
+          jQuery('.start_mp3').on('click',function(){
+            $('.start_mp3').hide();
+            $('.stop_mp3').show();
+            document.getElementById("song").play();
+          });
+          jQuery('.video').on('click',function(){
+            $('.start_mp3').show();
+            $('.stop_mp3').hide();
+            document.getElementById("song").pause();
+          });
+          jQuery('.vbox-close').on('click',function(){
+            $('.start_mp3').hide();
+            $('.stop_mp3').show();
+            document.getElementById("song").play();
+          });
+
+      </script>
+      <script>
+          $('.venobox').venobox({
+                numeratio: true,
+                border: '0px'
+              });
+         $('.venoboxframe').venobox({
+                border: '6px',
+                overlayColor : 'rgba(255,255,255,0.85)',
+                titlePosition : 'bottom',
+                titleColor: '#333',
+                titleBackground: 'transparent',
+                closeColor: '#333',
+                closeBackground: 'transparent',
+                        spinner : 'wave'
+            });
+         $('.venoboxvid').venobox({
+                bgcolor: '#000',
+                spinner : 'cube-grid',
+                // cb_post_open : function(obj, gallIndex, thenext, theprev){
+                //     console.log(thenext);
+                // },
+            });
+         $('.venoboxinline').venobox({
+            numeratio: true,
+                framewidth: '400px',
+                frameheight: 'auto',
+                border: '10px',
+                bgcolor: '#f46f00',
+                titleattr: 'data-title',
+                infinigall: true,
+                // cb_init : function(plugin){
+                //     console.log('INIT');
+                //     console.log(plugin);
+                // },
+                // cb_pre_open : function(obj){
+                //     console.log('link obj');
+                //     console.log(obj.data());
+                // },
+                // cb_post_open : function(obj, gallIndex, thenext, theprev){
+                //     console.log('item index');
+                //     console.log(gallIndex);
+                //     console.log('next - prev lenght');
+                //     console.log(thenext.length);
+                //     console.log(theprev.length);
+                // },
+                // cb_pre_close : function(obj, gallIndex, thenext, theprev){
+                //     console.log('item index');
+                //     console.log(gallIndex);
+                // },
+                // cb_post_close : function(){
+                //     console.log('CLOSED');
+                // },
+                // cb_after_nav : function(obj, gallIndex, thenext, theprev){
+                //     console.log('after nav');
+                //     console.log(gallIndex);
+                // }
+
+        })
+            </script>
+
+            <script type="text/javascript">
+              const scrollElements = document.querySelectorAll(".js-scroll");
+              const elementInView = (el, dividend = 1) => {
+                const elementTop = el.getBoundingClientRect().top;
+
+                return (
+                  elementTop <=
+                  (window.innerHeight || document.documentElement.clientHeight) / dividend
+                );
+              };
+              const elementOutofView = (el) => {
+                const elementTop = el.getBoundingClientRect().top;
+
+                return (
+                  elementTop > (window.innerHeight || document.documentElement.clientHeight)
+                );
+              };
+              const displayScrollElement = (element) => {
+                element.classList.add("scrolled");
+              };
+              const hideScrollElement = (element) => {
+                element.classList.remove("scrolled");
+              };
+              const handleScrollAnimation = () => {
+                scrollElements.forEach((el) => {
+                  if (elementInView(el, 1.25)) {
+                    displayScrollElement(el);
+                  } else if (elementOutofView(el)) {
+                    hideScrollElement(el)
+                  }
+                })
+              }
+              window.addEventListener("scroll", () => { 
+                handleScrollAnimation();
+              });
+            </script>
+  </footer>
+</body>
+</html>
